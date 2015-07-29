f3390143de10249f350b7411596470db5768252c
Edward Han <ehan1990@gmail.com>
2015-07-29 00:18:37 -0700

    clickable sidebar menu

--- dashboard/dist/angular/MainController.js
+++ dashboard/dist/angular/MainController.js
@@ -1,5 +1,12 @@
 var angularApp = angular.module('angularApp',[]);
 
-angularApp.controller('IndexController', function($scope) {
-    $scope.page = "template/home.html"
+angularApp.controller('IndexController', function($scope, $rootScope) {
+    $rootScope.page = "templates/match.html"
+});
+
+angularApp.controller('SidebarController', function($scope, $rootScope) {
+    $scope.setPage = function(pageName) {
+    	$rootScope.page = "templates/" + pageName + ".html";
+    	console.log("rootscope page: " + $rootScope.page);
+    }
 });
