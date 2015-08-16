fa5812c10a99e8e2fdf699979450c85a63f857ea
Edward Han <ehan1990@gmail.com>
2015-08-15 17:53:15 -0700

    added autocomplete example

--- dashboard/dist/angular/MainController.js
+++ dashboard/dist/angular/MainController.js
@@ -1,7 +1,21 @@
-var angularApp = angular.module('angularApp',[]);
+var angularApp = angular.module('angularApp',['autocomplete']);
 
 angularApp.controller('IndexController', function($scope, $rootScope) {
-    $rootScope.page = "templates/match.html"
+    $rootScope.page = "templates/home.html"
+    $scope.movies = ["Lord of the Rings",
+                        "Drive",
+                        "Science of Sleep",
+                        "Back to the Future",
+                        "Oldboy"];
+
+        // gives another movie array on change
+        $scope.updateMovies = function(typed){
+            // MovieRetriever could be some service returning a promise
+            $scope.newmovies = MovieRetriever.getmovies(typed);
+            $scope.newmovies.then(function(data){
+              $scope.movies = data;
+            });
+        }
 });
 
 angularApp.controller('SidebarController', function($scope, $rootScope) {
