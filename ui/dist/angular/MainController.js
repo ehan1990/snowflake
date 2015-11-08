2e19e147f7fc7d3cb28f062a6f4d70be0eb95424
Edward Han <ehan1990@gmail.com>
2015-11-08 01:26:41 -0800

    broke some stuff

---
+++ ui/dist/angular/MainController.js
@@ -0,0 +1,12 @@
+var angularApp = angular.module('angularApp');
+
+angularApp.controller('IndexController', function($scope, $rootScope) {
+    $rootScope.page = "templates/home.html"
+});
+
+angularApp.controller('SidebarController', function($scope, $rootScope) {
+    $scope.setPage = function(pageName) {
+    	$rootScope.page = "templates/" + pageName + ".html";
+    	console.log("rootscope page: " + $rootScope.page);
+    }
+});
