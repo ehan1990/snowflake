41026f119b2582e02422ea816a1d0636093f5b85
Edward Han <ehan@istuary.com>
2015-11-21 15:34:54 +0800

    removed unused file

--- src/model/api_call.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-
-url = 'http://localhost:1337/setup/api/time'
-payload = {'time': '2015-06-15T18:33:35Z'}
-headers = {'content-type': 'application/json'}
-
-response = requests.post(url, data=json.dumps(payload), headers=headers)
-print response
