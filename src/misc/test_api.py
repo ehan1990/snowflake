14e6013351b7be3ef5251c36d72ed0d39a13beeb
Edward Han <ehan@istuary.com>
2015-12-21 11:34:20 +0800

    removed unused module

--- src/misc/test_api.py
+++
@@ -1,9 +0,0 @@
-import requests, json
-
-import requests
-url = "http://192.168.0.79/api/v1/login"
-#data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
-response = requests.get(url)
-#print response
-print response.content
-print type(response.content)
