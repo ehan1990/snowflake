a20e3e6fcee29fb6d86f6b122aa06762a621e034
Edward Han <ehan1990@gmail.com>
2016-02-08 23:44:57 -0800

    changed file structure

--- ui/www/dist/asset/config.js
+++ ui/www/dist/asset/config.js
@@ -1,44 +1,44 @@
 app.config(function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/setting');
+    $urlRouterProvider.otherwise('/home');
     
     $stateProvider
         
         // HOME STATES AND NESTED VIEWS ========================================
         .state('zoo', {
             url: '/zoo',
-            templateUrl: 'views/zoosimulatorpage.html',
+            templateUrl: 'dist/asset/views/zoosimulatorpage.html',
             controller: 'ZooController'
         })
 
         .state('kafka_sender', {
             url: '/kafka_sender',
-            templateUrl: 'views/flowsimulatorpage.html',
+            templateUrl: 'dist/asset/views/flowsimulatorpage.html',
             controller: 'FlowController'
         })
 
         .state('kafka_reader', {
             url: '/kafka_reader',
-            templateUrl: 'views/kafkareaderpage.html',
+            templateUrl: 'dist/asset/views/kafkareaderpage.html',
             controller: 'KafkaReaderController'
         })
 
         .state('dpi', {
             url: '/dpi',
-            templateUrl: 'views/dpisimulatorpage.html',
+            templateUrl: 'dist/asset/views/dpisimulatorpage.html',
             controller: 'DpiController'
         })
 
         .state('scale_up', {
             url: '/scale',
-            templateUrl: 'views/scaleuppage.html',
+            templateUrl: 'dist/asset/views/scaleuppage.html',
             controller: 'ScaleUpController'
         })
 
-        .state('setting', {
-            url: '/setting',
-            templateUrl: 'views/settingpage.html',
-            controller: 'SettingController'
+        .state('home', {
+            url: '/home',
+            templateUrl: 'dist/asset/views/home.html',
+            controller: 'HomeController'
         })
 
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
