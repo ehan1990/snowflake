e681ef4028564f79423a22fd995679730003d3d7
Edward Han <ehan1990@gmail.com>
2016-02-09 00:32:06 -0800

    removed old stuff

--- ui/www/dist/asset/app.js
+++ ui/www/dist/asset/app.js
@@ -1,12 +1,6 @@
 
 var app = angular.module('CoolStuff', ['ui.router', 'autocomplete']);	
-// app.controller('ContentController', function($scope, $http) {
 
-// 	$scope.page = 'views/zoosimulatorpage.html';
-
-// 	$scope.setPage = function(pageName) {
-// 		console.log(pageName);
-// 		$scope.page = 'views/' + pageName + '.html';
-// 	}
-// });
-
+var constants = {
+	"rootApi": "api/v1"
+};
--- ui/www/dist/asset/config.js
+++ ui/www/dist/asset/config.js
@@ -7,52 +7,37 @@ app.config(function($stateProvider, $urlRouterProvider) {
         // HOME STATES AND NESTED VIEWS ========================================
         .state('zoo', {
             url: '/zoo',
-            templateUrl: 'dist/asset/views/zoosimulatorpage.html',
+            templateUrl: 'dist/asset/view/zoosimulatorpage.html',
             controller: 'ZooController'
         })
 
         .state('kafka_sender', {
             url: '/kafka_sender',
-            templateUrl: 'dist/asset/views/flowsimulatorpage.html',
+            templateUrl: 'dist/asset/view/flowsimulatorpage.html',
             controller: 'FlowController'
         })
 
         .state('kafka_reader', {
             url: '/kafka_reader',
-            templateUrl: 'dist/asset/views/kafkareaderpage.html',
+            templateUrl: 'dist/asset/view/kafkareaderpage.html',
             controller: 'KafkaReaderController'
         })
 
         .state('dpi', {
             url: '/dpi',
-            templateUrl: 'dist/asset/views/dpisimulatorpage.html',
+            templateUrl: 'dist/asset/view/dpisimulatorpage.html',
             controller: 'DpiController'
         })
 
         .state('scale_up', {
             url: '/scale',
-            templateUrl: 'dist/asset/views/scaleuppage.html',
+            templateUrl: 'dist/asset/view/scaleuppage.html',
             controller: 'ScaleUpController'
         })
 
         .state('home', {
             url: '/home',
-            templateUrl: 'dist/asset/views/home.html',
+            templateUrl: 'dist/asset/view/home.html',
             controller: 'HomeController'
-        })
-
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-            url: '/about',
-            views: {
-                '': { templateUrl: 'partial-about.html' },
-                'columnOne@about': { template: 'Look I am a column!' },
-                'columnTwo@about': { 
-                    templateUrl: 'table-data.html',
-                    controller: 'scotchController'
-                }
-            }
-            
-        });
-        
+        });        
 });
